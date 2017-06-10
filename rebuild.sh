36c729fb82d0f212a410fdb2442a880913c47fce
Andrey Verbin <info@studiomobile.ru>
2017-06-11 00:47:49 +0300

    Update to latest versions of Qoden projects

--- rebuild.sh
+++ rebuild.sh
@@ -1,6 +1,11 @@
 ﻿#!/bin/bash
 
-PROJECT=${PROJECT:-all}
+if [ -z "$1" ]; then
+    PROJECT=${PROJECT:-all}
+else 
+    PROJECT=$1
+fi
+
 CONFIG=${CONFIG:-Debug}
 REPO=`pwd`/local.nugets
 mkdir $REPO &> /dev/null
@@ -25,11 +30,7 @@ function rebuild()
 		echo "Error: '$1' not found";
 		exit 1;
 	fi
-
-	if [ -z "$SUFFIX" ]; then
-		local SUFFIX=b`git log --oneline | wc -l | tr -d ' '`
-	fi
-
+    
     if ! msbuild /t:restore; then
         echo "Error: package restore '$1' failed. Make sure you use latest nuget 'nuget update -self'";
         exit 1;
@@ -38,7 +39,11 @@ function rebuild()
 	if [[ "$1" == *iOS ]]; then	
 		echo "Info: iOS project detected. Using 'msbuild' and 'nuget' instead of 'dotnet' ==============="	
 
-		PACK_ARGS="-OutputDirectory bin/$CONFIG -Build -Properties Configuration=$CONFIG -Suffix $SUFFIX"
+        if [ ! -z "$SUFFIX" ]; then
+            $SUFFIX = "-Suffix $SUFFIX"
+        fi
+
+		PACK_ARGS="-OutputDirectory bin/$CONFIG -Build -Properties Configuration=$CONFIG $SUFFIX"
 		if ! nuget pack $PACK_ARGS; then
 			echo "Error: 'nuget pack $PACK_ARGS' failed"
 			exit 1;
@@ -46,9 +51,14 @@ function rebuild()
 	else
 		echo "Info: Normal project. Using 'dotnet' tools instead of 'msbuild' and 'nuget' ==============="	
 
-		PACK_ARGS="--version-suffix $SUFFIX -c $CONFIG"
+        if [ ! -z "$SUFFIX" ]; then
+            $SUFFIX = "--version-suffix  $SUFFIX"
+        fi
+
+		PACK_ARGS="$SUFFIX -c $CONFIG"
+        
 		if ! dotnet pack $PACK_ARGS ; then
-			echo "Error: 'dotnet pack --version-suffix $SUFFIX -c $CONFIG' in '$1' failed";
+			echo "Error: 'dotnet pack $SUFFIX -c $CONFIG' in '$1' failed";
 			exit 1;		
 		fi
 	fi
@@ -66,7 +76,7 @@ if [ "$PROJECT" == "all" ]; then
     if ! rebuild Qoden.Util/Qoden.Util; then exit 1; fi
     if ! rebuild Qoden.Validation/Qoden.Validation.AspNetCore; then exit 1; fi
 	if ! rebuild Qoden.Binding/Qoden.Binding; then exit 1; fi
-	if ! rebuild Qoden.UI/Qoden.UI.Abstractions; then exit 1; fi
+	if ! rebuild Qoden.UI/Qoden.UI; then exit 1; fi
 	if ! rebuild Qoden.UI/Qoden.UI.iOS; then exit 1; fi
 	if ! rebuild Qoden.SlideController/Qoden.SlideController.iOS; then exit 1; fi
 	if ! rebuild Qoden.Calendar/Qoden.Calendar.iOS; then exit 1; fi
